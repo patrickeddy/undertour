eaa62ae3450a28e7ba1d079a4af7dae7d20876f8
Patrick Eddy <patrickeddy@Patricks-MacBook-Pro.local>
2016-03-19 11:50:33 -0700

    Added method for fake data supplier.

--- app/src/main/java/com/patrickeddy/undertour/MainActivity.java
+++ app/src/main/java/com/patrickeddy/undertour/MainActivity.java
@@ -21,7 +21,7 @@ import com.patrickeddy.undertour.model.TourLocation;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener{
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
 
     private static final String APP_KEY = "DBbWOh4TzocBiVGaQ2wcdrdjDCqzsN1pAXHOZxO2";
     private static final String CLIENT_KEY = "01MuLrsIRrBJH6e3PuiMDE62eOP6wg1T87kJkDKe";
@@ -59,6 +59,65 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         // Register the Parse related classes.
         ParseObject.registerSubclass(TourLocation.class);
         ParseObject.registerSubclass(com.patrickeddy.undertour.model.Tour.class);
+
+        //FIXME: Remove this when actually using app
+        //Supply the DB with static data.
+//        fakeDatafy();
+    }
+
+    private void fakeDatafy() {
+        Tour testTour = new Tour();
+        testTour.setTitle("New Comer To Seattle");
+
+        final Tour theFinalTour = testTour;
+        testTour.saveInBackground(new SaveCallback() {
+            @Override
+            public void done(ParseException e) {
+                if (e!= null) {
+                    Log.e("SAMPLE-DATA", e.getMessage());
+                } else {
+                    try {
+                        addTourRelations(theFinalTour);
+                    } catch (ParseException e1) {
+                        e1.printStackTrace();
+                    }
+                }
+            }
+        });
+    }
+
+    private void addTourRelations(final Tour theTour) throws ParseException {
+        TourLocation l0 = new TourLocation();
+        TourLocation l1 = new TourLocation();
+        TourLocation l2 = new TourLocation();
+        TourLocation l3 = new TourLocation();
+        l0.setName("Kubota Garden");
+        l0.setCoordinate(new ParseGeoPoint(47.513084, -122.266741));
+        l0.save();
+        l1.setName("Westlake Park");
+        l1.setCoordinate(new ParseGeoPoint(47.611091, -122.337059));
+        l1.save();
+        l2.setName("Victor Steinbrueck Park");
+        l2.setCoordinate(new ParseGeoPoint(47.609977, -122.343691));
+        l2.save();
+        l3.setName("Kerry Park");
+        l3.setCoordinate(new ParseGeoPoint(47.629489, -122.359730));
+        l3.save();
+        theTour.addLocation(l0);
+        theTour.addLocation(l1);
+        theTour.addLocation(l2);
+        theTour.addLocation(l3);
+        theTour.saveInBackground(new SaveCallback() {
+            @Override
+            public void done(ParseException e) {
+                if (e != null) {
+                    Log.e("SAMPLE-DATA", e.getMessage());
+                } else {
+                    Log.d("SAMPLE-DATA", "Sample data uploaded.");
+                }
+            }
+        });
+
     }
 
     public void addTour(final View theView) {
--- app/src/main/java/com/patrickeddy/undertour/model/Tour.java
+++ app/src/main/java/com/patrickeddy/undertour/model/Tour.java
@@ -2,11 +2,35 @@ package com.patrickeddy.undertour.model;
 
 import com.parse.ParseClassName;
 import com.parse.ParseObject;
+import com.parse.ParseRelation;
 
 /**
  * Created by patrickeddy on 3/18/16.
  */
 @ParseClassName("Tour")
-public class Tour extends ParseObject{
+public class Tour extends ParseObject {
 
+    public String getTitle() {
+        return getString("title");
+    }
+
+    public ParseRelation getLocationRelation() {
+        return getRelation("locations");
+    }
+
+    public int getLikes() {
+        return getInt("likes");
+    }
+
+    public void setTitle(final String theTitle) {
+        put("title", theTitle);
+    }
+
+    public void addLocation(final TourLocation theLocation) {
+        getLocationRelation().add(theLocation);
+    }
+
+    public void removeLocation(final TourLocation theLocation) {
+        getLocationRelation().remove(theLocation);
+    }
 }
